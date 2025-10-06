372ced6f0c5ed2386ab257b32668b6e1de344e41
NJEEB <xxnjeebxx@gmail.com>
2025-10-06 13:50:18 +0300

    Add ServerName to apache2.conf to suppress warning

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -e
 # Use Railway's PORT or default to 80
 export PORT="${PORT:-80}"
 
+# Set ServerName globally to suppress Apache warning
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
 # Update Apache configuration with the actual PORT value
 sed -i "s/Listen 80/Listen $PORT/" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:80>/<VirtualHost *:$PORT>/" /etc/apache2/sites-available/000-default.conf
